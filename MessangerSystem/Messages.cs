c65b28718263d02364739802213f9f7b15caadd8
Nikita Goncharuk <retretret.r@mail.ru>
2018-09-17 20:37:47 +0300

    Commit 1.0 - Add Sample Scripts

---
+++ MessangerSystem/Messages.cs
@@ -0,0 +1,8 @@
+﻿
+public static class Messages
+{
+    public const string BALL_MISSED = "BALL_MISSED";
+    public const string BALL_COLLECTED = "BALL_COLLECTED";
+
+    public const string STARTUP_MESSAGE = "STARTUP_MESSAGE";
+}
---
+++ PoolableSystem/Prefab.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Prefab
+{
+    public GameObject prefab;
+    public int count;
+    public Transform parent;
+    
+    
+    public List<PoolablePrefabData> Install()
+    {
+        return PrefabPoolingSystem.Prespawn(prefab, count, parent);
+    }
+
+
+    public PoolablePrefabData Spawn()
+    {
+        return Spawn(Vector2.zero, Quaternion.identity);
+    }
+
+    public PoolablePrefabData Spawn(Vector2 position)
+    {
+        return Spawn(position, Quaternion.identity);
+    }
+
+    public PoolablePrefabData Spawn(Vector2 position, Quaternion rotation)
+    {
+        return PrefabPoolingSystem.Spawn(prefab, position, rotation);
+    }    
+}
